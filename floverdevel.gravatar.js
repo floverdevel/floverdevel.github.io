f0ac6b8e4b0e1682d80f5f6f23e4304788196e28
Ellis <eantaya@nuglif.com>
2021-02-06 03:57:29 -0500

    Gravatar

---
+++ floverdevel.gravatar.js
@@ -0,0 +1,30 @@
+// https://fr.gravatar.com/floverdevel.json
+const gravatar = {
+    "entry": [
+        {
+            "id": "33446608",
+            "hash": "7bc83bfc1a38bcb279466e11e877fc4e",
+            "requestHash": "floverdevel",
+            "profileUrl": "http:\/\/gravatar.com\/floverdevel",
+            "preferredUsername": "floverdevel",
+            "thumbnailUrl": "https:\/\/secure.gravatar.com\/avatar\/7bc83bfc1a38bcb279466e11e877fc4e",
+            "photos": [
+                {
+                    "value": "https:\/\/secure.gravatar.com\/avatar\/7bc83bfc1a38bcb279466e11e877fc4e",
+                    "type": "thumbnail"
+                }
+            ],
+            "profileBackground": {
+                "color": "#000000",
+                "url": "https:\/\/secure.gravatar.com\/bg\/33446608\/69b47f445a165c4abeff729021212606"
+            },
+            "displayName": "floverdevel",
+            "currentLocation": "Plan\u00e8te Terre, Voie Lact\u00e9e",
+            "urls": [
+                {"value": "https:\/\/nuglif.com", "title": "Nuglif"},
+                {                "value": "https:\/\/plus.lapresse.ca",
+                "title": "La Presse+"
+            }, {"value": "https:\/\/www.lapresse.ca", "title": "La Presse"}]
+        }
+    ]
+};
